f6783c790389d92cab8b3a0715e977e2aade5513
Evgeny Batalov <eugene_batalov@mail.ru>
2017-03-18 13:35:25 +0300

    BigInteger

---
+++ src/main/java/hackerrank/java/bignumber/BigDecimal.java
@@ -0,0 +1,40 @@
+package hackerrank.java.bignumber;
+
+import java.util.*;
+
+public class BigDecimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] s = new String[n + 2];
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next();
+        }
+        sc.close();
+        TreeMap<java.math.BigDecimal, ArrayList<Integer>> treeMap = new TreeMap<>(Collections.reverseOrder());
+        String[] s1 = new String[n];
+        for (int i = 0; i < n; i++) {
+            java.math.BigDecimal bigDecimal = new java.math.BigDecimal(s[i]);
+            if (!treeMap.containsKey(bigDecimal)) {
+                ArrayList<Integer> integers = new ArrayList<>();
+                integers.add(i);
+                treeMap.put(bigDecimal, integers);
+            } else {
+                ArrayList<Integer> integers = treeMap.get(bigDecimal);
+                integers.add(i);
+            }
+            s1[i] = s[i];
+        }
+        int k = 0;
+        for (Map.Entry<java.math.BigDecimal, ArrayList<Integer>> entry : treeMap.entrySet()) {
+            ArrayList<Integer> integers = entry.getValue();
+            for (Integer integer : integers) {
+                s[k] = s1[integer];
+                k++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(s[i]);
+        }
+    }
+}
---
+++ src/main/java/hackerrank/java/bignumber/BigInteger.java
@@ -0,0 +1,13 @@
+package hackerrank.java.bignumber;
+
+import java.util.Scanner;
+
+public class BigInteger {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        java.math.BigInteger bigInteger1 = in.nextBigInteger();
+        java.math.BigInteger bigInteger2 = in.nextBigInteger();
+        System.out.println(bigInteger1.add(bigInteger2));
+        System.out.println(bigInteger1.multiply(bigInteger2));
+    }
+}
